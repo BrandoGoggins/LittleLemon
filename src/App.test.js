c9997983726398acd1bea7364e3f797a0335026b
yungbrando <18bfengler@gmail.com>
2024-05-09 18:43:27 -0500

    added unit testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import BookingForm from "./components/BookingForm/BookingForm";
+import { initializeTimes, updateTimes } from "./pages/BookingPage";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("Renders the Choose Date Label", () => {
+  render(
+    <BookingForm
+      availableTimes={{
+        times: [],
+      }}
+      dispatch={expect.anything()}
+    />
+  );
+  const labelElement = screen.getByText("Choose date");
+  expect(labelElement).toBeInTheDocument();
+});
+
+test("initializeTimes returns the correct expected value", () => {
+  const today = new Date();
+  const initialState = initializeTimes();
+  const expectedResult = { times: fetchAPI(today) };
+  expect(initialState).toEqual(expectedResult);
+});
+
+test("updateTimes returns the same state", () => {
+  const state = {
+    times: ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"],
+  };
+  const action = { type: "SOME_ACTION" };
+  const newState = updateTimes(state, action);
+  expect(newState).toEqual(state);
 });
